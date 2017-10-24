0185dd1f029b0954e4eda4526dbb98db0bb50f15
Kapil Koju <kplx2.0@gmail.com>
2017-10-24 17:35:36 +0545

    Upgrade to boot 2 M5, java 9 and delomboked

--- src/main/java/com/kapilkoju/nepse/data/floorsheet/FloorSheetEntry.java
+++ src/main/java/com/kapilkoju/nepse/data/floorsheet/FloorSheetEntry.java
@@ -1,18 +1,220 @@
 package com.kapilkoju.nepse.data.floorsheet;
 
-import lombok.Builder;
-import lombok.Value;
-
 import java.math.BigDecimal;
 
-@Value
-@Builder
 class FloorSheetEntry {
-    private Long contractNo;
-    private String stockSymbol;
-    private Integer buyerBroker;
-    private Integer sellerBroker;
-    private Integer quantity;
-    private BigDecimal rate;
-    private BigDecimal amount;
+
+    private final Long contractNo;
+    private final String stockSymbol;
+    private final Integer buyerBroker;
+    private final Integer sellerBroker;
+    private final Integer quantity;
+    private final BigDecimal rate;
+    private final BigDecimal amount;
+
+    @java.beans.ConstructorProperties({"contractNo", "stockSymbol", "buyerBroker", "sellerBroker", "quantity", "rate", "amount"})
+    FloorSheetEntry(
+        Long contractNo,
+        String stockSymbol,
+        Integer buyerBroker,
+        Integer sellerBroker,
+        Integer quantity,
+        BigDecimal rate,
+        BigDecimal amount
+    ) {
+        this.contractNo = contractNo;
+        this.stockSymbol = stockSymbol;
+        this.buyerBroker = buyerBroker;
+        this.sellerBroker = sellerBroker;
+        this.quantity = quantity;
+        this.rate = rate;
+        this.amount = amount;
+    }
+
+    public Long getContractNo() {
+        return this.contractNo;
+    }
+
+    public String getStockSymbol() {
+        return this.stockSymbol;
+    }
+
+    public Integer getBuyerBroker() {
+        return this.buyerBroker;
+    }
+
+    public Integer getSellerBroker() {
+        return this.sellerBroker;
+    }
+
+    public Integer getQuantity() {
+        return this.quantity;
+    }
+
+    public BigDecimal getRate() {
+        return this.rate;
+    }
+
+    public BigDecimal getAmount() {
+        return this.amount;
+    }
+
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+        if (!(o instanceof FloorSheetEntry)) {
+            return false;
+        }
+        final FloorSheetEntry other = (FloorSheetEntry) o;
+        final Object this$contractNo = this.getContractNo();
+        final Object other$contractNo = other.getContractNo();
+        if (this$contractNo == null ? other$contractNo != null : !this$contractNo.equals(other$contractNo)) {
+            return false;
+        }
+        final Object this$stockSymbol = this.getStockSymbol();
+        final Object other$stockSymbol = other.getStockSymbol();
+        if (this$stockSymbol == null ? other$stockSymbol != null : !this$stockSymbol.equals(other$stockSymbol)) {
+            return false;
+        }
+        final Object this$buyerBroker = this.getBuyerBroker();
+        final Object other$buyerBroker = other.getBuyerBroker();
+        if (this$buyerBroker == null ? other$buyerBroker != null : !this$buyerBroker.equals(other$buyerBroker)) {
+            return false;
+        }
+        final Object this$sellerBroker = this.getSellerBroker();
+        final Object other$sellerBroker = other.getSellerBroker();
+        if (this$sellerBroker == null ? other$sellerBroker != null : !this$sellerBroker.equals(other$sellerBroker)) {
+            return false;
+        }
+        final Object this$quantity = this.getQuantity();
+        final Object other$quantity = other.getQuantity();
+        if (this$quantity == null ? other$quantity != null : !this$quantity.equals(other$quantity)) {
+            return false;
+        }
+        final Object this$rate = this.getRate();
+        final Object other$rate = other.getRate();
+        if (this$rate == null ? other$rate != null : !this$rate.equals(other$rate)) {
+            return false;
+        }
+        final Object this$amount = this.getAmount();
+        final Object other$amount = other.getAmount();
+        if (this$amount == null ? other$amount != null : !this$amount.equals(other$amount)) {
+            return false;
+        }
+        return true;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final Object $contractNo = this.getContractNo();
+        result = result * PRIME + ($contractNo == null ? 43 : $contractNo.hashCode());
+        final Object $stockSymbol = this.getStockSymbol();
+        result = result * PRIME + ($stockSymbol == null ? 43 : $stockSymbol.hashCode());
+        final Object $buyerBroker = this.getBuyerBroker();
+        result = result * PRIME + ($buyerBroker == null ? 43 : $buyerBroker.hashCode());
+        final Object $sellerBroker = this.getSellerBroker();
+        result = result * PRIME + ($sellerBroker == null ? 43 : $sellerBroker.hashCode());
+        final Object $quantity = this.getQuantity();
+        result = result * PRIME + ($quantity == null ? 43 : $quantity.hashCode());
+        final Object $rate = this.getRate();
+        result = result * PRIME + ($rate == null ? 43 : $rate.hashCode());
+        final Object $amount = this.getAmount();
+        result = result * PRIME + ($amount == null ? 43 : $amount.hashCode());
+        return result;
+    }
+
+    public String toString() {
+        return "FloorSheetEntry(contractNo=" +
+            this.getContractNo() +
+            ", stockSymbol=" +
+            this.getStockSymbol() +
+            ", buyerBroker=" +
+            this.getBuyerBroker() +
+            ", sellerBroker=" +
+            this.getSellerBroker() +
+            ", quantity=" +
+            this.getQuantity() +
+            ", rate=" +
+            this.getRate() +
+            ", amount=" +
+            this.getAmount() +
+            ")";
+    }
+
+    public static FloorSheetEntryBuilder builder() {
+        return new FloorSheetEntryBuilder();
+    }
+
+    public static class FloorSheetEntryBuilder {
+
+        private Long contractNo;
+        private String stockSymbol;
+        private Integer buyerBroker;
+        private Integer sellerBroker;
+        private Integer quantity;
+        private BigDecimal rate;
+        private BigDecimal amount;
+
+        FloorSheetEntryBuilder() {
+        }
+
+        public FloorSheetEntry.FloorSheetEntryBuilder contractNo(Long contractNo) {
+            this.contractNo = contractNo;
+            return this;
+        }
+
+        public FloorSheetEntry.FloorSheetEntryBuilder stockSymbol(String stockSymbol) {
+            this.stockSymbol = stockSymbol;
+            return this;
+        }
+
+        public FloorSheetEntry.FloorSheetEntryBuilder buyerBroker(Integer buyerBroker) {
+            this.buyerBroker = buyerBroker;
+            return this;
+        }
+
+        public FloorSheetEntry.FloorSheetEntryBuilder sellerBroker(Integer sellerBroker) {
+            this.sellerBroker = sellerBroker;
+            return this;
+        }
+
+        public FloorSheetEntry.FloorSheetEntryBuilder quantity(Integer quantity) {
+            this.quantity = quantity;
+            return this;
+        }
+
+        public FloorSheetEntry.FloorSheetEntryBuilder rate(BigDecimal rate) {
+            this.rate = rate;
+            return this;
+        }
+
+        public FloorSheetEntry.FloorSheetEntryBuilder amount(BigDecimal amount) {
+            this.amount = amount;
+            return this;
+        }
+
+        public FloorSheetEntry build() {
+            return new FloorSheetEntry(contractNo, stockSymbol, buyerBroker, sellerBroker, quantity, rate, amount);
+        }
+
+        public String toString() {
+            return "FloorSheetEntry.FloorSheetEntryBuilder(contractNo=" +
+                this.contractNo +
+                ", stockSymbol=" +
+                this.stockSymbol +
+                ", buyerBroker=" +
+                this.buyerBroker +
+                ", sellerBroker=" +
+                this.sellerBroker +
+                ", quantity=" +
+                this.quantity +
+                ", rate=" +
+                this.rate +
+                ", amount=" +
+                this.amount +
+                ")";
+        }
+    }
 }
--- src/main/java/com/kapilkoju/nepse/data/todaysprice/TodaysPriceEntry.java
+++ src/main/java/com/kapilkoju/nepse/data/todaysprice/TodaysPriceEntry.java
@@ -1,20 +1,287 @@
 package com.kapilkoju.nepse.data.todaysprice;
 
-import lombok.Builder;
-import lombok.Value;
-
 import java.math.BigDecimal;
 
-@Value
-@Builder
 class TodaysPriceEntry {
-    private String companyName;
-    private Integer noOfTransactions;
-    private BigDecimal maxPrice;
-    private BigDecimal minPrice;
-    private BigDecimal closingPrice;
-    private Integer tradedShares;
-    private BigDecimal amount;
-    private BigDecimal previousClosing;
-    private BigDecimal difference;
+    private final String companyName;
+    private final Integer noOfTransactions;
+    private final BigDecimal maxPrice;
+    private final BigDecimal minPrice;
+    private final BigDecimal closingPrice;
+    private final Integer tradedShares;
+    private final BigDecimal amount;
+    private final BigDecimal previousClosing;
+    private final BigDecimal difference;
+
+    @java.beans.ConstructorProperties({
+        "companyName",
+        "noOfTransactions",
+        "maxPrice",
+        "minPrice",
+        "closingPrice",
+        "tradedShares",
+        "amount",
+        "previousClosing",
+        "difference"
+    })
+    TodaysPriceEntry(
+        String companyName,
+        Integer noOfTransactions,
+        BigDecimal maxPrice,
+        BigDecimal minPrice,
+        BigDecimal closingPrice,
+        Integer tradedShares,
+        BigDecimal amount,
+        BigDecimal previousClosing,
+        BigDecimal difference
+    ) {
+        this.companyName = companyName;
+        this.noOfTransactions = noOfTransactions;
+        this.maxPrice = maxPrice;
+        this.minPrice = minPrice;
+        this.closingPrice = closingPrice;
+        this.tradedShares = tradedShares;
+        this.amount = amount;
+        this.previousClosing = previousClosing;
+        this.difference = difference;
+    }
+
+    public String getCompanyName() {
+        return this.companyName;
+    }
+
+    public Integer getNoOfTransactions() {
+        return this.noOfTransactions;
+    }
+
+    public BigDecimal getMaxPrice() {
+        return this.maxPrice;
+    }
+
+    public BigDecimal getMinPrice() {
+        return this.minPrice;
+    }
+
+    public BigDecimal getClosingPrice() {
+        return this.closingPrice;
+    }
+
+    public Integer getTradedShares() {
+        return this.tradedShares;
+    }
+
+    public BigDecimal getAmount() {
+        return this.amount;
+    }
+
+    public BigDecimal getPreviousClosing() {
+        return this.previousClosing;
+    }
+
+    public BigDecimal getDifference() {
+        return this.difference;
+    }
+
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+        if (!(o instanceof TodaysPriceEntry)) {
+            return false;
+        }
+        final TodaysPriceEntry other = (TodaysPriceEntry) o;
+        final Object this$companyName = this.getCompanyName();
+        final Object other$companyName = other.getCompanyName();
+        if (this$companyName == null ? other$companyName != null : !this$companyName.equals(other$companyName)) {
+            return false;
+        }
+        final Object this$noOfTransactions = this.getNoOfTransactions();
+        final Object other$noOfTransactions = other.getNoOfTransactions();
+        if (this$noOfTransactions == null ? other$noOfTransactions != null : !this$noOfTransactions.equals(other$noOfTransactions)) {
+            return false;
+        }
+        final Object this$maxPrice = this.getMaxPrice();
+        final Object other$maxPrice = other.getMaxPrice();
+        if (this$maxPrice == null ? other$maxPrice != null : !this$maxPrice.equals(other$maxPrice)) {
+            return false;
+        }
+        final Object this$minPrice = this.getMinPrice();
+        final Object other$minPrice = other.getMinPrice();
+        if (this$minPrice == null ? other$minPrice != null : !this$minPrice.equals(other$minPrice)) {
+            return false;
+        }
+        final Object this$closingPrice = this.getClosingPrice();
+        final Object other$closingPrice = other.getClosingPrice();
+        if (this$closingPrice == null ? other$closingPrice != null : !this$closingPrice.equals(other$closingPrice)) {
+            return false;
+        }
+        final Object this$tradedShares = this.getTradedShares();
+        final Object other$tradedShares = other.getTradedShares();
+        if (this$tradedShares == null ? other$tradedShares != null : !this$tradedShares.equals(other$tradedShares)) {
+            return false;
+        }
+        final Object this$amount = this.getAmount();
+        final Object other$amount = other.getAmount();
+        if (this$amount == null ? other$amount != null : !this$amount.equals(other$amount)) {
+            return false;
+        }
+        final Object this$previousClosing = this.getPreviousClosing();
+        final Object other$previousClosing = other.getPreviousClosing();
+        if (this$previousClosing == null ? other$previousClosing != null : !this$previousClosing.equals(other$previousClosing)) {
+            return false;
+        }
+        final Object this$difference = this.getDifference();
+        final Object other$difference = other.getDifference();
+        if (this$difference == null ? other$difference != null : !this$difference.equals(other$difference)) {
+            return false;
+        }
+        return true;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final Object $companyName = this.getCompanyName();
+        result = result * PRIME + ($companyName == null ? 43 : $companyName.hashCode());
+        final Object $noOfTransactions = this.getNoOfTransactions();
+        result = result * PRIME + ($noOfTransactions == null ? 43 : $noOfTransactions.hashCode());
+        final Object $maxPrice = this.getMaxPrice();
+        result = result * PRIME + ($maxPrice == null ? 43 : $maxPrice.hashCode());
+        final Object $minPrice = this.getMinPrice();
+        result = result * PRIME + ($minPrice == null ? 43 : $minPrice.hashCode());
+        final Object $closingPrice = this.getClosingPrice();
+        result = result * PRIME + ($closingPrice == null ? 43 : $closingPrice.hashCode());
+        final Object $tradedShares = this.getTradedShares();
+        result = result * PRIME + ($tradedShares == null ? 43 : $tradedShares.hashCode());
+        final Object $amount = this.getAmount();
+        result = result * PRIME + ($amount == null ? 43 : $amount.hashCode());
+        final Object $previousClosing = this.getPreviousClosing();
+        result = result * PRIME + ($previousClosing == null ? 43 : $previousClosing.hashCode());
+        final Object $difference = this.getDifference();
+        result = result * PRIME + ($difference == null ? 43 : $difference.hashCode());
+        return result;
+    }
+
+    public String toString() {
+        return "TodaysPriceEntry(companyName=" +
+            this.getCompanyName() +
+            ", noOfTransactions=" +
+            this.getNoOfTransactions() +
+            ", maxPrice=" +
+            this.getMaxPrice() +
+            ", minPrice=" +
+            this.getMinPrice() +
+            ", closingPrice=" +
+            this.getClosingPrice() +
+            ", tradedShares=" +
+            this.getTradedShares() +
+            ", amount=" +
+            this.getAmount() +
+            ", previousClosing=" +
+            this.getPreviousClosing() +
+            ", difference=" +
+            this.getDifference() +
+            ")";
+    }
+
+    public static TodaysPriceEntryBuilder builder() {
+        return new TodaysPriceEntryBuilder();
+    }
+
+    public static class TodaysPriceEntryBuilder {
+
+        private String companyName;
+        private Integer noOfTransactions;
+        private BigDecimal maxPrice;
+        private BigDecimal minPrice;
+        private BigDecimal closingPrice;
+        private Integer tradedShares;
+        private BigDecimal amount;
+        private BigDecimal previousClosing;
+        private BigDecimal difference;
+
+        TodaysPriceEntryBuilder() {
+        }
+
+        public TodaysPriceEntry.TodaysPriceEntryBuilder companyName(String companyName) {
+            this.companyName = companyName;
+            return this;
+        }
+
+        public TodaysPriceEntry.TodaysPriceEntryBuilder noOfTransactions(Integer noOfTransactions) {
+            this.noOfTransactions = noOfTransactions;
+            return this;
+        }
+
+        public TodaysPriceEntry.TodaysPriceEntryBuilder maxPrice(BigDecimal maxPrice) {
+            this.maxPrice = maxPrice;
+            return this;
+        }
+
+        public TodaysPriceEntry.TodaysPriceEntryBuilder minPrice(BigDecimal minPrice) {
+            this.minPrice = minPrice;
+            return this;
+        }
+
+        public TodaysPriceEntry.TodaysPriceEntryBuilder closingPrice(BigDecimal closingPrice) {
+            this.closingPrice = closingPrice;
+            return this;
+        }
+
+        public TodaysPriceEntry.TodaysPriceEntryBuilder tradedShares(Integer tradedShares) {
+            this.tradedShares = tradedShares;
+            return this;
+        }
+
+        public TodaysPriceEntry.TodaysPriceEntryBuilder amount(BigDecimal amount) {
+            this.amount = amount;
+            return this;
+        }
+
+        public TodaysPriceEntry.TodaysPriceEntryBuilder previousClosing(BigDecimal previousClosing) {
+            this.previousClosing = previousClosing;
+            return this;
+        }
+
+        public TodaysPriceEntry.TodaysPriceEntryBuilder difference(BigDecimal difference) {
+            this.difference = difference;
+            return this;
+        }
+
+        public TodaysPriceEntry build() {
+            return new TodaysPriceEntry(
+                companyName,
+                noOfTransactions,
+                maxPrice,
+                minPrice,
+                closingPrice,
+                tradedShares,
+                amount,
+                previousClosing,
+                difference
+            );
+        }
+
+        public String toString() {
+            return "TodaysPriceEntry.TodaysPriceEntryBuilder(companyName=" +
+                this.companyName +
+                ", noOfTransactions=" +
+                this.noOfTransactions +
+                ", maxPrice=" +
+                this.maxPrice +
+                ", minPrice=" +
+                this.minPrice +
+                ", closingPrice=" +
+                this.closingPrice +
+                ", tradedShares=" +
+                this.tradedShares +
+                ", amount=" +
+                this.amount +
+                ", previousClosing=" +
+                this.previousClosing +
+                ", difference=" +
+                this.difference +
+                ")";
+        }
+    }
 }
