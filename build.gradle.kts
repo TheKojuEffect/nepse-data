afaefdb9f61daba0b9d9f84270a756138a741431
Kapil Koju <kplx2.0@gmail.com>
2017-11-02 00:02:45 +0545

    Use gradle kotlin-dsl

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.1.51"
+    id("org.jetbrains.kotlin.plugin.spring") version "1.1.51"
+    id("org.springframework.boot") version "1.5.8.RELEASE"
+    id("io.spring.dependency-management") version "1.0.3.RELEASE"
+}
+
+group = "com.kapilkoju"
+version = "0.0.1-SNAPSHOT"
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+dependencies {
+    compile("org.springframework.boot:spring-boot-starter-web")
+    compile("org.jetbrains.kotlin:kotlin-stdlib-jre8")
+    compile("org.jetbrains.kotlin:kotlin-reflect")
+    compile("org.jsoup:jsoup:1.10.3")
+    testCompile("org.springframework.boot:spring-boot-starter-test")
+}
+
+repositories {
+    mavenCentral()
+    maven(url = "https://repo.spring.io/snapshot")
+    maven(url = "https://repo.spring.io/milestone")
+}
