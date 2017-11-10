afe2b09c1363e228c0d7d565209f46541d7a22e1
Kapil Koju <kplx2.0@gmail.com>
2017-11-10 08:37:12 +0545

    Release 17.11.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.kapilkoju"
-version = "17.11.05"
+version = "17.11.10"
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
